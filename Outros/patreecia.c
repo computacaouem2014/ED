82d0d1ad63d263c0c4f5c8444d768124e0261565
Tiago Nascimento <tiago@theosirian.com>
2015-09-17 23:30:19 -0300

    pretty print patreecia

--- Outros/patreecia.c
+++ Outros/patreecia.c
@@ -110,14 +110,14 @@ char first_different_bit(char a, char b){
 
 void ptree_insert(ptreenode **node, char valor){
 	if (ptree_is_empty(*node)) {
-		printf("Arvore Vazia\nCriando Node Externo.\n");
+		// printf("Arvore Vazia\nCriando Node Externo.\n");
 		*node = (ptreenode *) malloc(sizeof(ptreenode));
 		*node = (ptreenode*) malloc(sizeof(ptreenode));
 		(*node)->direita = (*node)->esquerda = 0;
 		(*node)->valor = valor;
 		printf("Elemento \'%c\' adicionado a arvore.\n", valor);
 	} else if ((*node)->esquerda == 0 && (*node)->direita == 0) {
-		printf("Encontrado Node Externo.\n");
+		// printf("Encontrado Node Externo.\n");
 		if (valor == (*node)->valor) {
 			printf("Erro! O elemento \'%c\' ja esta na arvore.\n", valor);
 		} else {
@@ -139,7 +139,7 @@ void ptree_insert(ptreenode **node, char valor){
 			printf("Elemento \'%c\' adicionado a arvore.\n", valor);
 		}
 	} else if ((*node)->esquerda != 0 && (*node)->direita != 0) {
-		printf("Passando por Node Interno.\n");
+		// printf("Passando por Node Interno.\n");
 		if (get_bit_at(valor, (*node)->valor) == 0) {
 			// ESQUERDA
 			ptree_insert(&((*node)->esquerda), valor);
@@ -213,9 +213,9 @@ void ptree_search(ptreenode ** node, char valor){
 			}
 		} else if ((*node)->direita == 0 && (*node)->esquerda == 0) {
 			if ((*node)->valor != valor) {
-				printf("Buscando(\'%c\')\nO elemento \'%c\' nao esta na arvore.\n", valor, valor);
+				printf("Buscando(\'%c\'): O elemento nao esta na arvore.\n", valor);
 			} else {
-				printf("Buscando(\'%c\')\nO elemento \'%c\' esta na arvore.\n", valor, valor);
+				printf("Buscando(\'%c\'): O elemento esta na arvore.\n", valor);
 			}
 		} else {
 			printf("Erro! Estrutura nao e uma arvore patricia.\n");
@@ -237,6 +237,41 @@ void ptree_print_in_order(ptreenode * node){
 	}
 }
 
+void ptree_pretty_print_internal(ptreenode * node, int is_right, int level, char *spaces){
+	if (node != 0) {
+		char rs[4 * level + 1], ls[4 * level + 1];
+		sprintf(ls, "%s%s", spaces == 0 ? "" : spaces, is_right ? "|   " : "    ");
+		sprintf(rs, "%s%s", spaces == 0 ? "" : spaces, is_right ? "    " : "|   ");
+		ptree_pretty_print_internal(node->esquerda, 0, level + 1, ls);
+		printf("%s", spaces == 0 ? "" : spaces);
+		if (node->esquerda != 0 && node->direita != 0) {
+			printf("%s%d\n", is_right ? "\\---" : "/---", node->valor);
+		} else if (node->esquerda == 0 && node->direita == 0) {
+			printf("%s%c\n", is_right ? "\\---" : "/---", node->valor);
+		}
+		ptree_pretty_print_internal(node->direita, 1, level + 1, rs);
+	}
+}
+
+void ptree_pretty_print(ptreenode * node){
+	if (node != 0) {
+		ptree_pretty_print_internal(node->esquerda, 0, 1, 0);
+		if (node->esquerda != 0 && node->direita != 0) {
+			printf("%d\n", node->valor);
+		} else if (node->esquerda == 0 && node->direita == 0) {
+			printf("%c\n", node->valor);
+		}
+		ptree_pretty_print_internal(node->direita, 1, 1, 0);
+	}
+}
+
+void print_spaces(int quantity){
+	if (quantity < 0) return;
+	for (int i = 0; i < quantity; i++) {
+		printf(" ");
+	}
+}
+
 void ptree_print_bfs(ptreenode * node){
 	if (!ptree_is_empty(node)) {
 		queue * q = 0;
@@ -245,21 +280,29 @@ void ptree_print_bfs(ptreenode * node){
 		int count = 1;
 		int next_count = 0;
 		ptreenode * current_node = 0;
+		int level = 0;
 		while (!queue_is_empty(q)) {
 			queue_dequeue(q, &current_node);
-			if (current_node->esquerda != 0 && current_node->direita != 0) {
+			print_spaces((int) floor((16 - level) / (2 * (level + 1))));
+			if (current_node == 0) {
+				printf("  ");
+			} else if (current_node->esquerda != 0 && current_node->direita != 0) {
 				printf("%d", current_node->valor);
 				queue_enqueue(q, current_node->esquerda);
 				queue_enqueue(q, current_node->direita);
-				next_count += 2;
 			} else if (current_node->esquerda == 0 && current_node->direita == 0) {
 				printf("%c", current_node->valor);
+				queue_enqueue(q, 0);
+				queue_enqueue(q, 0);
 			}
+			print_spaces((int) floor((16 - level) / (2 * (level + 1))));
+			next_count += 2;
 			count--;
 			if (count <= 0) {
 				printf("\n");
 				count = next_count;
 				next_count = 0;
+				level++;
 			}
 		}
 		queue_destroy(&q);
@@ -269,52 +312,36 @@ void ptree_print_bfs(ptreenode * node){
 
 int main() {
 	ptreenode * root = 0;
-	ptree_insert(&root, 'D');
-	printf("Arvore no passeio in-order: ");
-	ptree_print_in_order(root);
-	printf("\n\n");
-
-	ptree_insert(&root, 'B');
-	printf("Arvore no passeio in-order: ");
-	ptree_print_in_order(root);
-	printf("\n\n");
-
-	ptree_insert(&root, 'C');
-	printf("Arvore no passeio in-order: ");
-	ptree_print_in_order(root);
-	printf("\n\n");
-
-	ptree_insert(&root, 'C');
-	printf("Arvore no passeio in-order: ");
-	ptree_print_in_order(root);
-	printf("\n\n");
 
 	ptree_insert(&root, 'A');
-	printf("Arvore no passeio in-order: ");
-	ptree_print_in_order(root);
-	printf("\n\n");
+	ptree_insert(&root, 'B');
+	ptree_insert(&root, 'C');
+	ptree_insert(&root, 'D');
+	ptree_insert(&root, 'E');
+	ptree_insert(&root, 'F');
+	ptree_insert(&root, 'G');
+	ptree_insert(&root, 'H');
+	ptree_insert(&root, 'I');
+	ptree_insert(&root, 'J');
+	ptree_insert(&root, 'K');
+	ptree_insert(&root, 'L');
+	ptree_insert(&root, 'M');
+	ptree_insert(&root, 'a');
+	ptree_insert(&root, 'b');
+	ptree_insert(&root, 'c');
+	ptree_insert(&root, 'd');
+	ptree_insert(&root, 'e');
+	ptree_insert(&root, 'f');
+	ptree_insert(&root, 'g');
+	ptree_insert(&root, 'h');
+	ptree_insert(&root, 'i');
+	ptree_insert(&root, 'j');
+	ptree_insert(&root, 'k');
+	ptree_insert(&root, 'l');
+	ptree_insert(&root, 'm');
 
-	ptree_remove(&root, 'A');
-	printf("Arvore no passeio in-order: ");
-	ptree_print_in_order(root);
-	printf("\n\n");
-
-	ptree_search(&root, 'C');
 	printf("\n");
-
-	ptree_remove(&root, 'C');
-	printf("Arvore no passeio in-order: ");
-	ptree_print_in_order(root);
-	printf("\n\n");
-
-	ptree_remove(&root, 'C');
-	printf("Arvore no passeio in-order: ");
-	ptree_print_in_order(root);
-	printf("\n\n");
-
-	ptree_search(&root, 'C');
+	ptree_pretty_print(root);
 	printf("\n");
-
-	//ptree_print_bfs(root);
 	return 0;
 }
